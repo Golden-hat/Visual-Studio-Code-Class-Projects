09f673126732444eaf3ef620641448fdf1d56324
YassinPell <86885873+WontCompute@users.noreply.github.com>
2021-11-24 20:17:23 +0100

    commit

--- conjeturadecollatz.java
+++ conjeturadecollatz.java
@@ -1,23 +1,30 @@
 public class conjeturadecollatz{
     public static void main(String[] args) {
-        collatz(871);
+        collatz(217);
     }
     public static void collatz(long n){
         long currResult = n;
         int iteraciones = 0;
         System.out.println("0. "+n);
         do{
-            if(currResult%2 != 0){
+            if(n == 1){
                 ++iteraciones;
-                currResult = 3*currResult+1;
                 System.out.print(iteraciones+ ". ");
                 System.out.println(currResult);
             }
-            if(currResult%2 == 0){
-                ++iteraciones;
-                currResult = currResult/2;
-                System.out.print(iteraciones+ ". ");
-                System.out.println(currResult);
+            else{
+                if(currResult%2 != 0){
+                    ++iteraciones;
+                    currResult = 3*currResult+1;
+                    System.out.print(iteraciones+ ". ");
+                    System.out.println(currResult);
+                }
+                else{
+                    ++iteraciones;
+                    currResult = currResult/2;
+                    System.out.print(iteraciones+ ". ");
+                    System.out.println(currResult);
+                }
             }
         }while(currResult != 1);
 

2d2bfd1c40a7e3bbf35d8e5e7eb3781ecab3070f
WontCompute <86885873+WontCompute@users.noreply.github.com>
2021-11-26 13:19:03 +0100

    commit

--- BasicArrayNonInput.java
+++ BasicArrayNonInput.java
@@ -4,7 +4,7 @@ public class BasicArrayNonInput{
         int[] Array = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
             int sum = 0;
 
-        for ( int x= 0; x<Array.length; x++){
+        for ( int x= 0; x < Array.length; x++){
             sum = sum + Array[x];
          }
     

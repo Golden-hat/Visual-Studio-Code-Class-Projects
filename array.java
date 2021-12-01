1a6f28b719bc5ea85d8e3248257060c1778c5410
WontCompute <86885873+WontCompute@users.noreply.github.com>
2021-12-01 13:29:13 +0100

    commit

---
+++ array.java
@@ -0,0 +1,29 @@
+public class array {
+    public static void main(String[] args) {
+        int a[] = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+    }
+
+    public static void swap(int [] a, int j, int k){
+        int temp = a[j];
+        a[j] = a[k];
+        a[k] = temp;
+    }
+
+    public static void printArr(int [] a){
+        for (int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+
+    public static int maximum(int[] a){
+        int pos = 0;
+        
+        for(int i = 1; i<a.length; i++){
+            if(a[i] > a[pos]){
+                pos = i;
+            }
+        }
+        return pos;
+    }
+}

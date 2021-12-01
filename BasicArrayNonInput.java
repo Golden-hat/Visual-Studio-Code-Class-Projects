82ab5ab6824be1084a0dd759ffe95a5f819af571
WontCompute <86885873+WontCompute@users.noreply.github.com>
2021-12-01 11:39:29 +0100

    COMMIT

--- BasicArrayNonInput.java
+++ BasicArrayNonInput.java
@@ -1,11 +1,11 @@
 public class BasicArrayNonInput{
     public static void main(String Args[]){
         
-        int[] Array = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        int[] paquito = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
             int sum = 0;
 
-        for ( int x= 0; x < Array.length; x++){
-            sum = sum + Array[x];
+        for ( int x= 0; x < paquito.length; x++){
+            sum = sum + paquito[x];
          }
     
         System.out.println("La suma de todos los números de tu array es: " + sum);

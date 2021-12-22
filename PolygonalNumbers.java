266d215e9530ee5bc1ac4dad28c07bbdff87f426
WontCompute <86885873+WontCompute@users.noreply.github.com>
2021-12-22 12:06:23 +0100

    commit

---
+++ PolygonalNumbers.java
@@ -0,0 +1,16 @@
+public class PolygonalNumbers{
+    public static void main(String[] args) {
+        check(15, 3);
+    }
+
+    public static boolean check (int k, int s){
+        int n = 1;
+        int r;
+        do{
+            r = n*((s-2)*n-(s-4))/2;
+            n++;
+        }while(r < k);
+
+        return k==r;
+    }
+}

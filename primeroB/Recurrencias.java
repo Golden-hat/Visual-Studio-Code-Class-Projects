0b253b00fa614512595052a73ca632330bdba741
YassinPell <86885873+WontCompute@users.noreply.github.com>
2022-02-02 22:49:29 +0100

    commit

--- primeroB/Recurrencias.java
+++ primeroB/Recurrencias.java
@@ -1,7 +1,8 @@
 package primeroB;
 public class Recurrencias{
+
     public static void main(String[] args) {
-        System.out.println(fibonacci(13));
+       System.out.println(DivSubstraction(50, 5)); 
     }
 
     public static int fibonacci(int n){
@@ -13,6 +14,46 @@ public class Recurrencias{
         }
         else{
             return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+
+    public static int factorial(int n){
+        if(n == 0){
+            return 0;
+        }
+        else if (n == 1){    
+            return 1;
+        }
+        else{
+            return n * factorial(n-1);
+        } 
+    }
+    
+    public static int printNum(int n){
+        if(n == 0){
+            return 0;
+        }
+        else{
+            System.out.println(n);
+            return printNum(n-1);
+        }
+    }
+
+    public static int DivSubstraction(int a, int b){
+        if(a < b){
+            return 0;
+        }
+        else{
+            return DivSubstraction(a-b, b) + 1;
+        }
+    }
+
+    public static int MultAddition(int a, int b){
+        if (b == 0){
+            return 0;
+        }
+        else{
+            return MultAddition(a, (b-1)) + a;
         }
     }
 }

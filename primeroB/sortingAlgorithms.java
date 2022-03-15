6d9a764f003715752e277003336a7c5d6d620f89
WontCompute <86885873+WontCompute@users.noreply.github.com>
2022-03-15 12:20:58 +0100

    commt

---
+++ primeroB/sortingAlgorithms.java
@@ -0,0 +1,21 @@
+package primeroB;
+public class sortingAlgorithms {
+    public static void main(String[] args) {
+        
+    }
+
+    private static int[] extract(int[] a, int ini, int fin){
+        int[] temp = new int [ini - fin];
+        for(int i = 0; i < temp.length; i++){
+            
+        }
+    }
+
+    public static void MergeSort(int v[]){
+        if(v.length > 2){
+            int k = v.length/2;
+            int left[] = extract(v, 0, k);
+            int right[] = extract(v, k, v.length);
+        }
+    }
+}

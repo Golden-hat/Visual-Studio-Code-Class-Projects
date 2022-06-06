3932e7f02813f429630ab6aad86d9ebb6b72ac4f
WontCompute <86885873+WontCompute@users.noreply.github.com>
2022-06-06 12:53:34 +0200

    commit

--- primeroB/LinkedSequences/QueueIntLinked/QueueIntLinked.java
+++ primeroB/LinkedSequences/QueueIntLinked/QueueIntLinked.java
@@ -2,8 +2,17 @@ package primeroB.LinkedSequences.QueueIntLinked;
 
 public class QueueIntLinked {
 
-    
-    public QueueIntLinked(){
+    public int size;
+    public NodeInt4Queues first;
+    public NodeInt4Queues last;
 
+    public QueueIntLinked(){
+        this.size = 0;
+        this.first = null;
+        this.last = null;
+    }
+
+    public void addToQueue(){
+        
     }
 }

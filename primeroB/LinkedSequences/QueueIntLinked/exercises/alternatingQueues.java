ccdbd1ec4be4f8865b1dda96d7b538ba8e88a474
YassinPell <86885873+WontCompute@users.noreply.github.com>
2022-06-07 02:14:35 +0200

    commit

--- primeroB/LinkedSequences/QueueIntLinked/exercises/alternatingQueues.java
+++ primeroB/LinkedSequences/QueueIntLinked/exercises/alternatingQueues.java
@@ -1,4 +1,5 @@
 package primeroB.LinkedSequences.QueueIntLinked.exercises;
+import primeroB.LinkedSequences.QueueIntLinked.NodeInt4Queues;
 import primeroB.LinkedSequences.QueueIntLinked.QueueIntLinked;
 
 public class alternatingQueues {
@@ -10,9 +11,34 @@ public class alternatingQueues {
         int t[] = {10, 9, 8};
         n.ArrayAddToQueue(s);
         k.ArrayAddToQueue(t);
-        n.addToQueue(5);
-        k.addToQueue(6);
         System.out.println(n);
-        System.out.println(k);    
+        System.out.println(k);
+        System.out.println(fusion(n, k)); 
+    }
+
+    public static QueueIntLinked fusion(QueueIntLinked q1, QueueIntLinked q2){
+        QueueIntLinked aux = new QueueIntLinked();
+        int minimum = Math.min(q1.size(), q2.size());
+
+        NodeInt4Queues firstq1 = q1.first;
+        NodeInt4Queues firstq2 = q2.first;
+
+        for(int i = 0; i < minimum; i++){
+            aux.addToQueue(firstq1.data);
+            aux.addToQueue(firstq2.data);
+            firstq1 = firstq1.getNext();
+            firstq2 = firstq2.getNext();
+        }
+        while(minimum < q1.size()){
+            aux.addToQueue(firstq1.data);
+            firstq1 = firstq1.getNext();
+            minimum++;
+        }
+        while(minimum < q2.size()){
+            aux.addToQueue(firstq2.data);
+            firstq2 = firstq2.getNext();
+            minimum++;
+        }
+        return aux;
     }
 }

eadcbee5097af1e26c016ced6117e38eb9e65c3b
Golden_hat <yassinpellicerlamla@gmail.com>
2022-11-30 16:35:22 +0100

    commit

---
+++ segundoA/RedPracs/prac7/daytimeUDP.java
@@ -0,0 +1,18 @@
+package prac7;
+import java.net.*;
+import java.util.*;
+
+public class daytimeUDP extends Thread{
+    public static void main(String[] args) throws Exception{
+        byte[] buffer = new byte[1000];
+        DatagramPacket p = new DatagramPacket(buffer, 1000);
+        DatagramSocket ds = new DatagramSocket(7778);
+        ds.receive(p);
+        Date now = new Date();
+        String now_string = now.toString() + "\r+\n";
+        p.setData(now_string.getBytes());
+        p.setLength(now_string.getBytes().length);
+        ds.send(p);
+        ds.close();
+    }
+}

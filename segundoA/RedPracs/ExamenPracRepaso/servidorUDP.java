5722943847f0f58f282ad468871c6d768b2b4328
Yassin <yassinpellicerlamla@gmail.com>
2022-12-19 00:25:45 +0100

    commit

---
+++ segundoA/RedPracs/ExamenPracRepaso/servidorUDP.java
@@ -0,0 +1,16 @@
+package ExamenPracRepaso;
+import java.io.*;
+import java.net.*;
+
+public class servidorUDP {
+    public static void main(String[] args) throws IOException {
+        try(DatagramSocket s = new DatagramSocket(7777)){        
+            DatagramPacket p = new DatagramPacket(new byte[512], 512);
+            while(true){
+                s.receive(p); // se bloquea hasta que recibe un datagrama
+                s.send(p);
+            }
+        }
+
+    }
+}

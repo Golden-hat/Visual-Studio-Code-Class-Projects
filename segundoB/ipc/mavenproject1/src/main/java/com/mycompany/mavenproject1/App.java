572bb1727a731ff6bc97b01ce767ecb4a274f483
Yassin Pellicer Lamla <yassinpellicerlamla@gmail.com>
2023-02-10 19:52:18 +0100

    commit

--- segundoB/ipc/mavenproject1/src/main/java/com/mycompany/mavenproject1/App.java
+++ segundoB/ipc/mavenproject1/src/main/java/com/mycompany/mavenproject1/App.java
@@ -21,6 +21,7 @@ public class App extends Application {
         var scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(scene);
         stage.show();
+        //ijasdfuahbouahbfguabf
     }
 
     public static void main(String[] args) {

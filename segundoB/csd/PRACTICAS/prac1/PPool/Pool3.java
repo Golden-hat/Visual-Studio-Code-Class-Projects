c0a8f54095e72300ba22103839cf5954c96d6690
yassin <yassinpellicerlamla@gmail.com>
2023-02-21 18:11:46 +0100

    commit

--- segundoB/csd/PRACTICAS/prac1/PPool/Pool3.java
+++ segundoB/csd/PRACTICAS/prac1/PPool/Pool3.java
@@ -1,9 +1,44 @@
 // CSD feb 2015 Juansa Sendra
 
 public class Pool3 extends Pool{ //max capacity
-    public void init(int ki, int cap)           {}
-    public void kidSwims()      {log.swimming();}
-    public void kidRests()      {log.resting(); }
-    public void instructorSwims()   {log.swimming();}
-    public void instructorRests()   {log.resting(); }
+    public int insNum = 0;
+    public int kidNum = 0;
+    public int KI = 0;
+    public int max = 0;
+    public void init(int ki, int cap){this.KI = ki; this.max = cap;}
+    public synchronized void kidSwims() throws InterruptedException{
+        while((insNum <= 0 || kidNum >= this.KI*insNum) && (insNum + kidNum >= max - 1)){
+            log.waitingToSwim();
+            wait();
+        }
+        kidNum++;
+        log.swimming();
+        notifyAll();
+    }
+    public synchronized void kidRests() throws InterruptedException{
+        kidNum--;
+        log.resting();
+        notifyAll();
+    }
+    public synchronized void instructorSwims() throws InterruptedException{
+        while((insNum + kidNum) >= max - 1){
+            log.waitingToSwim();
+            wait();
+        }
+        insNum++;
+        log.swimming();
+        notifyAll();
+    }
+    public synchronized void instructorRests() throws InterruptedException{
+        while((kidNum > 0 && insNum < 2) || kidNum >= this.KI*(insNum-1)){
+            if (kidNum < 1) { // si es 0>=
+                break;
+            }
+            log.waitingToRest();
+            wait();
+        }
+        insNum--;
+        log.resting();
+        notifyAll();
+    }
 }

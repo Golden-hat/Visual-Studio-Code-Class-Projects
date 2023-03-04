75d9a73ce16cc4e650f41c4c5663b896731c25d0
Yassin <yassinpellicerlamla@gmail.com>
2023-03-04 21:05:48 +0100

    commit

--- segundoB/EDAsegundo/eda/topic2/ejercicios2.java
+++ segundoB/EDAsegundo/eda/topic2/ejercicios2.java
@@ -7,12 +7,14 @@ public class ejercicios2{
         Integer[] mergeS = {0,1,5,7,19,52,2,3,6,3,4,7,345,678,24523};
 
         int[] a = {-5, -4, -3, -2, -1, 1, 2, 3, 4};
-        int[] b = {33,10,8,0,1,9};
+        int[] b = {33,10,8,1532,11,9235,25,56,12345,7456,2,45,26,47356,236,4768,45,-5634,-37};
 
         System.out.println(arrayIntegersCurveLinear(a));
         System.out.println(arrayIntegersCurveLog(a));
         System.out.println(concaveCurve(b));
-        mergeSort(mergeS);
+        printArr(b);
+        printArr(mergeSortTwo(b));
+        printArr(b);
         printArr(mergeS);
     }
     
@@ -65,24 +67,77 @@ public class ejercicios2{
         }
     }
 
-    public static <T extends Comparable<T>> void mergeSort(T[] array){
-        mergeSort(array, 0, array.length-1);
+    /* MERGE SORT WITH INTEGER ARRAYS */
+
+    public static void mergeSort(int[] a){
+        mergeSort(a, 0, a.length-1);
     }
 
-    private static <T extends Comparable<T>> void mergeSort(T[] array, int left, int right){
-
-        if(left < right){
-            int m = (left + right)/2;
-            mergeSort(array, 0, m);
-            mergeSort(array, m+1, right);
-
-            mergeDC(array, left, m+1, right); 
+    private static void mergeSort(int[] a, int ini, int fin){
+        int mid = (ini+fin)/2;
+        if(ini < fin){
+            mergeSort(a, ini, mid);
+            mergeSort(a, mid+1, fin);
+            merge(a, ini, mid, fin);
         }
-        if (array.length < 2) {
+        else{
             return;
         }
     }
-    
+
+    private static void merge(int[] a, int ini, int mid, int fin){
+        int[] aux = new int[fin-ini+1];
+        int i = ini, j = mid+1, k = 0;
+        while(i <= mid && j <= fin){
+            if(a[i] < a[j]){
+                aux[k++] = a[i++];
+            }
+            else{
+                aux[k++] = a[j++];
+            }
+        }
+        for(int r = i; r <= mid; r++){aux[k++] = a[r];}
+        for(int r = j; r <= fin; r++){aux[k++] = a[r];}
+
+        k = 0;
+        for(i = ini; i <= fin; i++){
+            a[i] = aux[k];
+            k++;
+        }
+    }
+
+    public static int[] mergeSortTwo(int[] a){
+        return mergeSortTwo(a, 0, a.length-1);
+    }
+
+    private static int[] mergeSortTwo(int[] a, int ini, int fin){
+        int mid = (ini+fin)/2;
+        if(ini < fin){
+            mergeSortTwo(a, ini, mid);
+            mergeSortTwo(a, mid+1, fin);
+            mergeTwo(a, a);
+        }
+        return a;
+    }
+
+    private static int[] mergeTwo(int[] a, int[] b){
+        int[] aux = new int[a.length + b.length];
+        int i = 0, j = 0, k = 0;
+        while(i < a.length && j < b.length){
+            if(a[i] < b[j]){
+                aux[k++] = a[i++];
+            }
+            else{
+                aux[k++] = b[j++];
+            }
+        }
+        for(int r = i; r < a.length; r++){aux[k++] = a[r];}
+        for(int r = j; r < b.length; r++){aux[k++] = b[r];}
+        return aux;
+    }
+
+    /* MERGE SORT WITH GENERIC ARRAYS */
+
     /* Given 2 sorted arrays in ascending order, this methods merges them in order */
     public static <E extends Comparable<E>> E[] merge(E[] a, E[] b){
         E[] res = (E[]) new Comparable[a.length + b.length];
@@ -100,31 +155,14 @@ public class ejercicios2{
         return res;
     }
 
-    public static <E extends Comparable<E>> void mergeDC(E[] a, int left, int mid, int right){
-        E[] res = (E[]) new Comparable[a.length];
-        
-        E[] izq = (E[]) new Comparable[a.length/2+1];
-        E[] der = (E[]) new Comparable[a.length/2+1];
-
-        for(int i = 0; i < a.length/2 + 1; i++){
-            izq[i] = a[i];
-            der[i] = a[a.length/2 + i];
+    /* This method prints integer type arrays */
+    public static void printArr(int[] a){
+        System.out.print("{");
+        for (int i = 0; i < a.length-1; i++){
+            System.out.print(a[i]+", ");
         }
-        
-        // Esta primera parte se encarga de meter los numeros en orden ascendente de cada array
-        // hasta que alguno de los dos se extenúe.
-        int i = 0, j = 0, k = 0;
-        while (i < izq.length && j < der.length) {
-            if (izq[i].compareTo(der[j]) < 0) res[k++] = izq[i++];
-            else res[k++] = der[j++];
-        }
-        // Esta segunda parte completa con los números faltantes del array no extenuado.
-        for (int r = i; r < izq.length; r++) res[k++] = izq[r];
-        for (int r = j; r < der.length; r++) res[k++] = der[r];
-
-        for(i = 0; i < res.length + 1; i++){
-            a[i] = res[i];
-        }
+        System.out.print(a[a.length-1]);
+        System.out.println("}");
     }
 
     /* This method prints Comparable type arrays */

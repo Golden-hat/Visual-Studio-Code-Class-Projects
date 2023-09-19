32c962fa77542ef9fa38c6af7da80aa53453cf87
Yassin Pellicer <yassinpellicerlamla@gmail.com>
2023-09-19 17:42:30 +0200

    commit

---
+++ terceroA/1ercuatri/ISW/CSHARP/LinkedList/LinkedList.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+
+
+static void Main(){
+    Node<int> n = new Node<int>();
+    n.data = 1;
+    n.next = null;
+}
---
+++ terceroA/1ercuatri/ISW/CSHARP/LinkedList/Node.cs
@@ -0,0 +1,6 @@
+using System;
+
+public class Node<T>{
+    public T data;
+    public Node<T> next;
+}

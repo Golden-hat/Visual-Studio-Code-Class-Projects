f4210f5368ee7fb3b2aab0d0242aa6a134d7a50d
Yassin Pellicer <86885873+Golden-hat@users.noreply.github.com>
2023-09-19 18:33:17 +0200

    commit

--- terceroA/1ercuatri/ISW/CSHARP/LinkedList/LinkedList.cs
+++ terceroA/1ercuatri/ISW/CSHARP/LinkedList/LinkedList.cs
@@ -1,9 +1,46 @@
 ﻿using System;
 
+class LinkedList<T>{
+    private Node<T> head;
 
+    public void Add(T n){
+        if(this.head == null){
+            Node<T> node = new Node<T>();
+            node.data = n;
+            node.next = null;
 
-static void Main(){
-    Node<int> n = new Node<int>();
-    n.data = 1;
-    n.next = null;
+            this.head = node;
+        }
+        else{
+            Node<T> node = new Node<T>();
+
+            node.data = n;
+            node.next = this.head;
+            this.head = node;
+        }
+    }
+
+    public void printList(){
+        Node<T> aux = this.head;
+
+        if(aux == null){Console.WriteLine("{}"); return;}
+        
+        Console.Write("{");
+        while(aux.next != null){
+            Console.Write(aux.data+"-> ");
+            aux = aux.next;
+        }
+        Console.WriteLine(aux.data+"}");
+    }
+
+    public void Insert(){
+
+    }
+}
+
+public class main{
+    public static void Main(){
+        LinkedList<int> n = new LinkedList<int>();
+        n.printList();
+    }
 }

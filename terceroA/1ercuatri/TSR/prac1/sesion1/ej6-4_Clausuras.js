2c592d6a6d2ac5136d3661bc1a49bf5b6396e453
yassin <yassinpellicerlamla@gmail.com>
2023-10-10 13:35:09 +0200

    commit

--- terceroA/1ercuatri/TSR/prac1/sesion1/ej6-4_Clausuras.js
+++ terceroA/1ercuatri/TSR/prac1/sesion1/ej6-4_Clausuras.js
@@ -7,21 +7,23 @@ function main(y) {
 
 		console.log("\ttraza: ", traza);
 
-		return (x % 2) ? g0 : g1;
-
 		function g0() {
+			console.log("hola desde g0")
 			traza += "-g0";
 			x++;
 			console.log("g0: incremento de x:  " + x);
 			return f(++y);
 		}
 
-		function g1() {
+		function g1() {	
+			console.log("hola desde g1")
 			traza += "-g1";
 			y++;
 			console.log("g1: incremento de y:  " + y);
 			return f(y);
-		}
+		}		
+		
+		return (x % 2) ? g0 : g1;
 	}
 }
 
@@ -30,9 +32,3 @@ var func = main(-100);
 for (let i = 0; i < 10; i++) {
 	func = func();
 }
-
-/**
-	Cuestión 1. Cuál es la clausura de la función g0
-
-la forma las variables traza y var x
- */
